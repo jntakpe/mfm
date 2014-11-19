b953ebe24fae2198a97701ea91563e6755c4cf60
jntakpe <j.ntakpe@gmail.com>
2014-11-19 22:53:22 +0100

    Initialisation du modèle de données

---
+++ src/main/java/com/github/jntakpe/mfm/model/UpdateRate.java
@@ -0,0 +1,19 @@
+package com.github.jntakpe.mfm.model;
+
+/**
+ * Taux de mise à jour d'un projet
+ *
+ * @author jntakpe
+ */
+public enum UpdateRate {
+
+    SECOND,
+    FIVE_SECOND,
+    TEN_SECOND,
+    HALF_SECONDS,
+    MINUTE,
+    TEN_MINUTE,
+    HALF_HOUR,
+    HOUR,
+    DAY
+}

a8ea37cc27ca9106498f717da7cf99c778e445aa
jntakpe <j.ntakpe@gmail.com>
2014-11-21 23:06:48 +0100

    Menu en AngularJS avec des directives pour les sous menus

--- src/main/webapp/static/js/menu.js
+++ src/main/webapp/static/js/menu.js
@@ -3,5 +3,33 @@ var menuApp = angular.module('menuApp', []);
 menuApp.controller('MenuCtrl', MenuCtrl);
 
 function MenuCtrl() {
-
+    this.active = {
+        main: 'projects',
+        sub: ''
+    };
+    this.changeMainMenu = function (main) {
+        if (main !== this.active.main) {
+            this.active.sub = '';
+        }
+        this.active.main = main;
+    };
 }
+
+menuApp.directive('sub', function SubMenu() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                description: '@',
+                active: '='
+            },
+            link: function (scope, element, attributes) {
+                scope.application = attributes.application;
+                element.bind('click', function () {
+                    scope.active.sub = scope.application;
+                });
+            },
+            templateUrl: 'directives/sub-menu.html'
+        };
+    }
+);
